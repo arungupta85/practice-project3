fa94c3ca1520efb1f323393b36b55f2a5736b026
Arun <arun.sdtc@gmail.com>
2024-12-24 23:23:05 +0530

    msg

---
+++ src/test/java/com/demo3/practice_project3/PracticeProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo3.practice_project3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
